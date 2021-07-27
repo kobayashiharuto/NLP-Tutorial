999d0f8d9bb2d771dc12ce300388336c40bfc5cb
kl <datesaver1234@gmail.com>
2021-07-27 21:10:42 +0900

    lstm success

--- lib/LSTM/lstm_gensim.py
+++ lib/LSTM/lstm_gensim.py
@@ -1,19 +1,19 @@
-from gensim.models import keyedvectors
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional, SpatialDropout1D
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.optimizers import Adam
 import matplotlib.pyplot as plt
 import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.util import ngrams
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 import numpy as np
 from sklearn.model_selection import train_test_split
 import gensim.downloader as api
 from tqdm import tqdm
-from gensim.models import KeyedVectors
+from tensorflow.keras.initializers import Constant
+
 
 # nltk.download('punkt')
 
@@ -36,8 +36,8 @@ def texts_to_words(texts):
     stop = stopwords.words('english')
     corpus = []
     for text in tqdm(texts):
-        words = [word.lower() for word in word_tokenize(text) if(
-            (word.isalpha() == 1) & (word not in stop))]
+        words = [word.lower() for word in word_tokenize(
+            text) if word.isalpha() & (word not in stop)]
         corpus.append(words)
     return corpus
 
@@ -45,38 +45,64 @@ def texts_to_words(texts):
 # 文章を単語リスト変換
 corpus = texts_to_words(df['text'])
 
-# word2vecの辞書データをダウンロード
+# word2vecの辞書データをロード
 wv = api.load('word2vec-google-news-300')
 
-# シーケンス番号に対しての辞書を作成する
-vocabulary = wv.key_to_index
-tokenizer = Tokenizer(num_words=len(vocabulary))
-tokenizer.word_index = vocabulary
-sequences = tokenizer.texts_to_sequences(corpus)
+# テキストの単語を扱いやすいようにシーケンス番号に変換
+tokenizer_obj = Tokenizer()
+tokenizer_obj.fit_on_texts(corpus)
+sequences = tokenizer_obj.texts_to_sequences(corpus)
 
-print(tokenizer.texts_to_sequences(['i', 'am', 'note']))
+# 単語 : シーケンス番号の辞書
+word_index = tokenizer_obj.word_index
 
-wv.save_word2vec_format('w2v/word2vec.kv')
-wv = KeyedVectors.load_word2vec_format('wv2/word2vec.kv')
-# keras の embedding層に変換
-embedding_layer = wv.get_keras_embedding()
+# ユニークな単語数を取得しておく
+num_words = len(word_index) + 1
+
+# 埋め込み用のベクトルを用意
+embedding_matrix = np.zeros((num_words, 300))
+
+# 単語を回しながらシーケンス番号に対応したベクトル表現のリストを作成する
+for word, i in tqdm(word_index.items()):
+    try:
+        emb_vec = wv.get_vector(word)
+    except KeyError:
+        pass
+    else:
+        embedding_matrix[i] = emb_vec
+
 
 # padding処理。50単語に満たないものは0埋めする。
-texts_pad = pad_sequences(sequences, maxlen=50,
+MAX_LEN = 50
+texts_pad = pad_sequences(sequences, maxlen=MAX_LEN,
                           truncating='post', padding='post')
 
 # 埋め込み層を含むモデルを作成
 model = Sequential([
-    embedding_layer,
+    Embedding(num_words, 300, embeddings_initializer=Constant(
+        embedding_matrix), input_length=MAX_LEN, trainable=False),
     SpatialDropout1D(0.2),
-    LSTM(64),
+    Bidirectional(LSTM(64, return_sequences=True)),
+    Bidirectional(LSTM(128)),
     Dense(1, activation='sigmoid')
 ])
 
 optimzer = Adam(learning_rate=1e-5)
 
-model.compile(loss='binary_crossentropy',
-              optimizer=optimzer, metrics=['accuracy'])
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=optimzer, metrics=['accuracy'],
+    callbacks=[
+        EarlyStopping(monitor='loss', min_delta=0,
+                      patience=20, verbose=1),
+        ReduceLROnPlateau(monitor='val_acc',
+                          patience=3,
+                          verbose=1,
+                          factor=0.5,
+                          min_lr=0.00001),
+        ModelCheckpoint('models/w2v_model.h5', save_best_only=True)
+    ],
+)
 
 model.summary()
 
