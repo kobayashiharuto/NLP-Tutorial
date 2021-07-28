a78d273eef3bc3bcf119a01582af24128376c926
kl <datesaver1234@gmail.com>
2021-07-29 03:15:09 +0900

    add comment

--- lib/BERT/bert.py
+++ lib/BERT/bert.py
@@ -6,32 +6,44 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
+
+# データを読み込む
 data = pd.read_csv('data/treated.csv')
 
+# 訓練用データを取得
 train = data.dropna(subset=['target'])
 train_text, train_label = train['text'], train['target']
 
+# テスト用データを取得
 test = data[data.isnull().any(1)]
 test_text = test['text']
 
+# 訓練用データを訓練用と評価用に分ける
 x_train, x_val, y_train, y_val =\
     train_test_split(train_text, train_label, test_size=0.15)
 
+# 各データをDatasetに変換
 train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
 val_ds = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+test_ds = tf.data.Dataset.from_tensor_slices((test_text))
+
+# データをバッチ化
 train_ds = train_ds.batch(10)
 val_ds = val_ds.batch(10)
 
 for feat, targ in train_ds.take(5):
     print('Features: {}, Target: {}'.format(feat, targ))
 
+# BERT用のレイヤーを読み込む
 preprocessing_layer = hub.KerasLayer(
     "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
 encoder = hub.KerasLayer(
     "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/2",
     trainable=True)
 
+# モデル構築
 input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
 encoder_inputs = preprocessing_layer(input)
 outputs = encoder(encoder_inputs)
@@ -44,29 +56,29 @@ model.summary()
 
 model.compile(
     loss='binary_crossentropy',
-    optimizer=Adam(learning_rate=1e-6),
+    optimizer=Adam(learning_rate=3e-6),
     metrics=['accuracy']
 )
 
 history = model.fit(
     train_ds,
-    epochs=10,
+    epochs=1000,
     validation_data=val_ds,
-    # callbacks=[
-    #     EarlyStopping(monitor='loss', min_delta=0,
-    #                   patience=20, verbose=1),
-    #     ReduceLROnPlateau(monitor='val_acc',
-    #                       patience=3,
-    #                       verbose=1,
-    #                       factor=0.5,
-    #                       min_lr=0.00001),
-    #     ModelCheckpoint('models/best_v10.h5', save_best_only=True)
-    # ],
+    callbacks=[
+        EarlyStopping(monitor='loss', min_delta=0,
+                      patience=10, verbose=1),
+        ReduceLROnPlateau(monitor='val_accuracy',
+                          patience=3,
+                          verbose=1,
+                          factor=0.5,
+                          min_lr=0.0000001),
+        ModelCheckpoint('models/bert.h5', save_best_only=True)
+    ],
 )
 
 # 推論
-predict = model.predict(test_text)
-opredict = np.round(predict).astype(int).reshape(3263)
+predict = model.predict(test_ds)
+opredict = np.round(predict).astype(int).reshape(-1)
 test_df = pd.read_csv('data/test.csv')
 sub = pd.DataFrame({'id': test_df['id'].values.tolist(), 'target': predict})
 sub.to_csv('result/result_bert.csv', index=False)
